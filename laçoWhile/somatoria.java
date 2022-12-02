de44b0ed591fe40e029f2ffa61fcf7eeba32b5ca
Mabelly <111589048+Mthiss@users.noreply.github.com>
2022-12-01 22:50:35 -0300

    Create somatoria.java

---
+++ laçoWhile/somatoria.java
@@ -0,0 +1,13 @@
+package exercico3;
+
+public class somatoria {
+
+	public static void main(String[] args) {
+		int i=1, a;
+		while (i<=100) {
+			a = (i+i);
+		i = i+i;
+		System.out.println(a);
+		}
+	}
+}
