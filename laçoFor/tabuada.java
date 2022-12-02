75bdcfb3054a327efc3cebe6c9c4c32773288a9d
Mabelly <111589048+Mthiss@users.noreply.github.com>
2022-12-01 22:50:06 -0300

    Create tabuada.java

---
+++ laçoFor/tabuada.java
@@ -0,0 +1,15 @@
+package exercicio1;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		for (int n = 1; n <= 10; n++){
+			System.out.println("            Tabuaba do " + n);
+			for (int i = 1; i <=10; i++) {
+			System.out.println(n + " X " + i + " = " + (n*i));
+			}
+
+			}
+	}
+
+}
